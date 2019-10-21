4d1dcec97873b36fd606008d133c7ee548945d2b
Nolte <noltesz@almbp-02011230.niehs.nih.gov>
2019-10-20 23:05:20 -0400

    clean up

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
  
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var jsonParser = bodyParser.json();
 const express = require('express');
-//const connect = require('connect');
 const path = require('path');//sue
 
 var exphbs = require('express-handlebars');
 const app = express();
-//const app = connect();
-// const path = require("path");
-//
-// express.set('views', path.join(_dirname, "views"))
-// express.set("view engine", "hbs")
-app.use(function middleware1(req, res, next) {
+
+/***app.use(function middleware1(req, res, next) {
   // middleware 1
   next();
 });
@@ -21,41 +15,20 @@ app.use(function middleware2(req, res, next) {
   // middleware 2
   next();
 });
-
-
+***/
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-//app.use(bodyParser.json());
-//app.use(express.bodyParser());
 
 
 app.engine('handlebars', exphbs({defaultView: 'main'}));
-//sue app.set('view engine', 'handlebars');
+
 app.set('view engine', 'hbs');
 // route for homepage
 app.get('/', function (req, res) {
-  //res.render('main', {layout: 'hello',title: 'My first handlebar get()'})
   res.render('index', {title: 'My first handlebar'})
 })
 
-/***app.post('/chatbot.php', function (req, res) {
-    res.render('main', {layout: 'chatbot.php'})
-})
-
-app.post('/', function (req, res) {
-	//Grab the request body
-    var body = req.body;
-
-    var res_body = {
-        title: body.title,
-        labname:body.labname
-        //name: body.name_1
-	};
-    //res.render('sue', {layout: 'hello',body:res_body})
-    res.render('sue', res_body);
-})
-***/
 app.post('/', function(req, res){
     var body = req.body;
     if (!body) return res.sendStatus(400);
@@ -72,5 +45,5 @@ app.post('/', function(req, res){
 
 module.exports = app;
 
-const port = 5001
+const port = 5001 
 app.listen(port,() => console.log(`Example app listening on port ${port}!`))
